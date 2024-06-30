718c741dbb30a3f54d46acba6019f31821d73c99
Pratik Mishra <116499548+Pratik-Xig@users.noreply.github.com>
2024-06-30 22:07:37 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,41 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event EtherSent(address recipient, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -48,13 +38,24 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function sendEther(address payable _recipient, uint256 _amount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(balance >= _amount, "Insufficient balance to send");
+        uint _previousBalance = balance;
+        (bool success,  = _recipient.call{value: _amount}("");
+        require(success, "Transfer failed");
+        balance -= _amount;
+        assert(balance == (_previousBalance - _amount));
+        emit EtherSent(_recipient, _amount);
+    }
+
+    receive() external payable {
+        balance += msg.value;
+        emit Deposit(msg.value);
     }
 }
